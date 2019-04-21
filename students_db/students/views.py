0e4133dbf5ae377f8a5e39b5755c0473add5bb2d
Evgeny_Skotarenko <jeny797@gmail.com>
2019-04-21 08:27:32 +0300

    created dynamic urls in students list and group list

--- students_db/students/views.py
+++ students_db/students/views.py
@@ -1,31 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html')
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'static/assets/images/smiley-1635449_640.png'},
+        {'id': 2,
+         'first_name': u'Корост',
+         'last_name': u'Андрій',
+         'ticket': 2123,
+         'image': 'static/assets/images/smiley-1635449_640.png'}
+    )
+    return render(request, 'students/students_list.html',
+                  {'students': students})
+
 
 def students_add(request):
     return HttpResponse('<h1>Student add Form</h1>')
 
+
 def students_edit(request, sid):
-    return HttpResponse('<h1>Edit Student %s</h1>' %sid)
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
 
 def students_delete(request, sid):
-    return HttpResponse('<h1>Delete Student %s</h1>' %sid)
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
 
 # Views for Group
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups listing</h1>')
+    groups = (
+        {'id': 1,
+         'number': u'МтМ-21',
+         'star': u'Ячменев Олег'},
+        {'id': 2,
+         'number': u'МтМ-22',
+         'star': u'Подоба Віталій'}
+    )
+    return render(request, 'students/groups.html', {'groups': groups})
+
 
 def groups_add(requests):
     return HttpResponse('<h1>Groups add Form</h1>')
 
+
 def groups_edit(request, gid):
-    return  HttpResponse('<h1>Edit Group %s</h1>' %gid)
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
 
 def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Student %s</h1>' %gid)
+    return HttpResponse('<h1>Delete Student %s</h1>' % gid)
--- students_db/students_db/urls.py
+++ students_db/students_db/urls.py
@@ -30,10 +30,12 @@ name='students_delete'),
     # Groups urls
     url(r'^groups/$', views.groups_list, name='groups'),
     url(r'^groups/add/$', views.groups_add, name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$', views.groups_edit,
+    url(r'^groups/(?P<gid>.+)/edit/$', views.groups_edit,
 name='groups_edit'),
-    url(r'’^groups/(?P<gid>\d+)/delete/$', views.groups_delete,
+    url(r'^groups/(?P<gid>.+)/delete/$', views.groups_delete,
 name='groups_delete'),
+
+    #
 
     url(r'^admin/', admin.site.urls),
 ]
